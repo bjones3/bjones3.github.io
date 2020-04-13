da92355a36efd81c7a821c9d0e2c76402dacc0bc
Brian Jones <bjones033@gmail.com>
2020-04-12 23:37:40 -0500

    Add commenting

--- connectFour/connectfour.js
+++ connectFour/connectfour.js
@@ -1,3 +1,4 @@
+// Creates the table grid and appends a grid square image to each td element
 function createTable() {
     for (let row = 0; row < 6; row++) {
         let board = document.getElementById("board");
@@ -15,14 +16,10 @@ function createTable() {
 };
 createTable();
 
-let turn = "yellow";
-let boardState = [[0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0],
-                  [0,0,0,0,0,0,0]];
-
+/*
+ * Checks for a win in every direction, given the location of last played token.
+ * @return true if a win, false otherwise.
+*/
 function testWinCondition(row, col) {
     let t = boardState[row][col]; //last played token
     let bs = boardState;
@@ -71,8 +68,9 @@ function testWinCondition(row, col) {
         return (true);
     else 
         return (false);
-}
+};
 
+//Displays message of who won, given who's turn it would be.
 function printWinner() {
     let winner = document.getElementById("winner");
     if (turn == "yellow") {
@@ -84,6 +82,7 @@ function printWinner() {
     }
 }
 
+//Highlights the playable cell within the same column of mouseEvent
 function highlightCell(mouseEvent) {
     if (mouseEvent.target.tagName != "TD")
         return;
@@ -106,6 +105,11 @@ function highlightCell(mouseEvent) {
     highlight.style.visibility = "visible";
 }
 
+/*
+ * Adds proper colored token to the playable cell within the same column of mouseEvent
+ * Tests for win condition after placing a token.
+ * If game won: remove event listeners to prevent further play; print winner
+*/
 function addToken(mouseEvent) {
     if (mouseEvent.target.tagName != "TD")
         return;
@@ -130,7 +134,6 @@ function addToken(mouseEvent) {
     }
     if (testWinCondition(row, col))
     {
-        console.log("winnawinna");
         document.getElementById("board").removeEventListener("mouseover", highlightCell);
         document.removeEventListener("click", addToken);
         printWinner();
@@ -139,10 +142,27 @@ function addToken(mouseEvent) {
     highlightCell(mouseEvent);
 }
 
+let turn = "yellow"; //current player's turn
+/*
+ * boardState represents the HTML table.
+ * Each value represents type of game piece at that location on the board.
+ * 0 = empty slot; 1 = yellow piece; 2 = red piece
+*/ 
+let boardState = [[0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0],
+                  [0,0,0,0,0,0,0]];
+
+/*
+ * EVENT LISTENERS:
+ *      - Highlight playable cell on mouseover.
+ *      - Add token to playable cell on click.
+ *      - Unhighlight playable cell upon leaving game board.
+*/
 document.getElementById("board").addEventListener("mouseover", highlightCell);
-
 document.addEventListener("click", addToken);
-
 document.getElementById("board").addEventListener("mouseleave", function() {
     document.getElementById("highlight").style.visibility = "hidden";
 });
